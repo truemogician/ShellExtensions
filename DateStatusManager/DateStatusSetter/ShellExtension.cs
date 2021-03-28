a03e0ff139549043141188f187e5bdfc5ac58e2c
true_mogician <truemogician@gmail.com>
2021-03-28 23:20:55 +0800

    feat(DateStatusSetter): property page

---
+++ DateStatusManager/DateStatusSetter/ShellExtension.cs
@@ -0,0 +1,14 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpPropertySheet;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace DateStatusSetter {
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.AllFilesAndFolders)]
+    public class PropertySheet : SharpPropertySheet {
+        protected override bool CanShowSheet() => true;
+        protected override IEnumerable<SharpPropertyPage> CreatePages() => new[] { new DateStatusPropertyPage(string.IsNullOrEmpty(FolderPath) ? SelectedItemPaths.ToArray() : new[] { FolderPath }) };
+    }
+}
