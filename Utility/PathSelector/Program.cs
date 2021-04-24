7a95de5d14e059c319e7d145dc89b2e72968b6ce
true_mogician <truemogician@gmail.com>
2021-04-24 16:35:08 +0800

    feat(PathSelector): create PathSelector

---
+++ Utility/PathSelector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace PathSelector {
+
+	public class Program {
+		public static void Main(string[] args) {
+			if (args == null || args.Length == 0)
+				return;
+			foreach (string arg in args)
+				if (!File.Exists(arg) && !Directory.Exists(arg)) {
+					MessageBox.Show($"Path {arg} doesn't exist.", "Path Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+			ExplorerSelector.FilesOrFolders(args);
+		}
+	}
+}
