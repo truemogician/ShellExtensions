54ca4ebcab2029c470772c30d608308efbabe612
true_mogician <925269713@qq.com>
2021-10-12 08:54:25 +0800

    feat(project): BatchRenamer

---
+++ FilePropertyManager/BatchRenamer/ContextMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using BatchRenamer.Windows;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+
+namespace BatchRenamer {
+	[ComVisible(true)]
+	[COMServerAssociation(AssociationType.AllFilesAndFolders)]
+	public class FilesAndFoldersMenu : SharpContextMenu {
+		protected override bool CanShowMenu() => true;
+
+		protected override ContextMenuStrip CreateMenu()
+			=> new() {
+				Items = {
+					new ToolStripMenuItem(
+						"批量重命名",
+						null,
+						(_, _) => new MainWindow(SelectedItemPaths).Show()
+					)
+				}
+			};
+	}
+
+	[ComVisible(true)]
+	[COMServerAssociation(AssociationType.DirectoryBackground)]
+	public class DirectoryBackgroundMenu : SharpContextMenu {
+		protected override bool CanShowMenu() => true;
+
+		protected override ContextMenuStrip CreateMenu()
+			=> new() {
+				Items = {
+					new ToolStripMenuItem(
+						"批量重命名",
+						null,
+						(_, _) => new MainWindow(Directory.GetFileSystemEntries(FolderPath)).Show()
+					)
+				}
+			};
+	}
+}
