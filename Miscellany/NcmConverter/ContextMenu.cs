55889e679613cd10edad6d3281f79ba0d987705e
true_mogician <truemogician@gmail.com>
2021-12-29 20:49:42 +0800

    feat(NcmConverter): add new project NcmConverter

---
+++ Miscellany/NcmConverter/ContextMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+
+namespace NcmConverter {
+	[ComVisible(true)]
+	[COMServerAssociation(AssociationType.AllFiles)]
+	public class ContextMenu : SharpContextMenu {
+		private const string ConverterPath = @"D:\Program Files\NcmDump\ncmdump.exe";
+
+		protected override bool CanShowMenu() => SelectedItemPaths.All(path => Path.GetExtension(path) == ".ncm");
+
+		protected override ContextMenuStrip CreateMenu() => new() {
+			Items = {
+				new ToolStripMenuItem(
+					"转换为mp3",
+					null,
+					(_, _) => Process.Start(
+						new ProcessStartInfo(ConverterPath, string.Join(" ", SelectedItemPaths.Select(p => $"\"{p}\""))) {
+							UseShellExecute = false,
+							CreateNoWindow = true
+						}
+					)
+				)
+			}
+		};
+	}
+}
