77b68b5d45596888290c92217dde21702f9f4db7
true_mogician <925269713@qq.com>
2022-05-06 00:01:03 +0800

    feat: SymbolLinkTool

---
+++ Miscellany/SymbolLinkTool/ContextMenu.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+using SymbolLinkTool.Utilities;
+using TrueMogician.Extensions.Enumerable;
+
+namespace SymbolLinkTool {
+	[ComVisible(true)]
+	[COMServerAssociation(AssociationType.AllFiles)]
+	public class FileContextMenu : SharpContextMenu {
+		protected override bool CanShowMenu() =>
+			SelectedItemPaths.All(p => !File.GetAttributes(p).HasFlag(FileAttributes.Directory));
+
+		protected override ContextMenuStrip CreateMenu() =>
+			new() {
+				Items = {
+					new ToolStripMenuItem(
+						"创建Hard Link",
+						Resource.Icon,
+						(_, _) => {
+							var paths = SelectedItemPaths.ToList();
+							if (paths.Count == 1) {
+								string src = paths[0];
+								var dialog = new SaveFileDialog {
+									Title = "选择要创建的Hard Link文件",
+									FileName = Path.GetFileName(src)
+								};
+								var result = dialog.ShowDialog();
+								if (result != DialogResult.OK)
+									return;
+								while (true) {
+									try {
+										HardLink.Create(dialog.FileName, src);
+									}
+									catch (Exception ex) {
+										if (MessageBox.Show(ex.Message, "Hard Link创建失败", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry)
+											continue;
+									}
+									break;
+								}
+							}
+							else {
+								if (!paths.Unique(Path.GetFileName)) {
+									MessageBox.Show("所选文件中存在重名", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+									return;
+								}
+								var dialog = new CommonOpenFileDialog {
+									IsFolderPicker = true,
+									Multiselect = false,
+									EnsurePathExists = true,
+									AllowNonFileSystemItems = false,
+									Title = "选择Hard Link文件的保存目录",
+									InitialDirectory = Path.GetDirectoryName(paths[0])
+								};
+								if (dialog.ShowDialog() != CommonFileDialogResult.Ok)
+									return;
+								while (true) {
+									try {
+										string root = dialog.FileName;
+										foreach (string path in paths)
+											HardLink.Create(Path.Combine(root, Path.GetFileName(path)), path);
+									}
+									catch (Exception ex) {
+										if (MessageBox.Show(ex.Message, "Hard Link创建失败", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry)
+											continue;
+									}
+									break;
+								}
+							}
+						}
+					)
+				}
+			};
+	}
+
+	[ComVisible(true)]
+	[COMServerAssociation(AssociationType.Folder)]
+	public class FolderContextMenu : SharpContextMenu {
+		protected override bool CanShowMenu() => !SelectedItemPaths.Skip(1).Any();
+
+		protected override ContextMenuStrip CreateMenu() =>
+			new() {
+				Items = {
+					new ToolStripMenuItem(
+						"创建Junction Point",
+						null,
+						(_, _) => {
+							string src = SelectedItemPaths.First();
+							var dialog = new CommonOpenFileDialog {
+								IsFolderPicker = true,
+								Multiselect = false,
+								EnsureFileExists = false,
+								EnsurePathExists = false,
+								Title = "选择要创建的Junction Point的空文件夹",
+								DefaultFileName = Path.GetFileName(src),
+								AllowNonFileSystemItems = false
+							};
+							string dst;
+							while (true) {
+								if (dialog.ShowDialog() != CommonFileDialogResult.Ok)
+									return;
+								dst = dialog.FileName;
+								DialogResult result;
+								if (dst == src)
+									result = MessageBox.Show("目标文件夹不可与源文件夹相同", "错误", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+								else if (Directory.EnumerateFileSystemEntries(dst).Any())
+									result = MessageBox.Show("目标文件夹非空", "错误", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+								else
+									break;
+								if (result != DialogResult.Retry)
+									return;
+							}
+							while (true) {
+								try {
+									JunctionPoint.Create(src, dst, true);
+								}
+								catch (IOException ex) {
+									string message = ex.InnerException?.Message ?? ex.Message;
+									if (MessageBox.Show(message, "Junction Point创建失败", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry)
+										continue;
+								}
+								break;
+							}
+						}
+					)
+				}
+			};
+	}
+
+	internal static class Resource {
+		internal static Image Icon = Resize(new Bitmap(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Icon.png")), 24, 24);
+
+		private static Image Resize(Image src, int width, int height) {
+			var destRect = new Rectangle(0, 0, width, height);
+			var destImage = new Bitmap(width, height);
+
+			destImage.SetResolution(src.HorizontalResolution, src.VerticalResolution);
+
+			using var graphics = Graphics.FromImage(destImage);
+			graphics.CompositingMode = CompositingMode.SourceCopy;
+			graphics.CompositingQuality = CompositingQuality.HighQuality;
+			graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+			graphics.SmoothingMode = SmoothingMode.HighQuality;
+			graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+			using var wrapMode = new ImageAttributes();
+			wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+			graphics.DrawImage(src, destRect, 0, 0, src.Width, src.Height, GraphicsUnit.Pixel, wrapMode);
+
+			return destImage;
+		}
+	}
+}
---
+++ Miscellany/SymbolLinkTool/Utilities/HardLink.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+using FileTime = System.Runtime.InteropServices.ComTypes.FILETIME;
+
+namespace SymbolLinkTool.Utilities {
+	public static class HardLink {
+		[DllImport("Kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		private static extern bool CreateHardLink(
+			string lpFileName,
+			string lpExistingFileName,
+			IntPtr lpSecurityAttributes
+		);
+
+		[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		private static extern SafeFileHandle CreateFile(
+			string lpFileName,
+			[MarshalAs(UnmanagedType.U4)] FileAccess dwDesiredAccess,
+			[MarshalAs(UnmanagedType.U4)] FileShare dwShareMode,
+			IntPtr lpSecurityAttributes,
+			[MarshalAs(UnmanagedType.U4)] FileMode dwCreationDisposition,
+			[MarshalAs(UnmanagedType.U4)] FileAttributes dwFlagsAndAttributes,
+			IntPtr hTemplateFile
+		);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		private static extern bool GetFileInformationByHandle(SafeFileHandle handle, out ByHandleFileInformation lpFileInformation);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool CloseHandle(SafeHandle hObject);
+
+		[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		private static extern IntPtr FindFirstFileNameW(
+			string lpFileName,
+			uint dwFlags,
+			ref uint stringLength,
+			StringBuilder fileName
+		);
+
+		[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		private static extern bool FindNextFileNameW(
+			IntPtr hFindStream,
+			ref uint stringLength,
+			StringBuilder fileName
+		);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		private static extern bool FindClose(IntPtr fFindHandle);
+
+		[DllImport("kernel32.dll")]
+		private static extern bool GetVolumePathName(
+			string lpszFileName,
+			[Out] StringBuilder lpszVolumePathName,
+			uint cchBufferLength
+		);
+
+		[DllImport(@"shlwapi.dll", CharSet = CharSet.Auto)]
+		private static extern bool PathAppend([In] [Out] StringBuilder pszPath, string pszMore);
+
+		public static void Create(string hardLinkPath, string sourcePath) {
+			if (!CreateHardLink(hardLinkPath, sourcePath, IntPtr.Zero))
+				throw Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
+		}
+
+		public static int GetFileLinkCount(string filepath) {
+			var result = 0;
+			var handle = CreateFile(filepath, FileAccess.Read, FileShare.Read, IntPtr.Zero, FileMode.Open, FileAttributes.Archive, IntPtr.Zero);
+			if (GetFileInformationByHandle(handle, out var fileInfo))
+				result = (int)fileInfo.NumberOfLinks;
+			CloseHandle(handle);
+			return result;
+		}
+
+		public static string[] GetFileSiblingHardLinks(string filePath) {
+			var result = new List<string>();
+			uint stringLength = 256;
+			var sb = new StringBuilder(256);
+			GetVolumePathName(filePath, sb, stringLength);
+			var volume = sb.ToString();
+			sb.Length = 0;
+			stringLength = 256;
+			var findHandle = FindFirstFileNameW(filePath, 0, ref stringLength, sb);
+			if (findHandle.ToInt32() != -1) {
+				do {
+					var pathSb = new StringBuilder(volume, 256);
+					PathAppend(pathSb, sb.ToString());
+					result.Add(pathSb.ToString());
+					sb.Length = 0;
+					stringLength = 256;
+				} while (FindNextFileNameW(findHandle, ref stringLength, sb));
+				FindClose(findHandle);
+				return result.ToArray();
+			}
+			return null;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct ByHandleFileInformation {
+			public uint FileAttributes;
+
+			public FileTime CreationTime;
+
+			public FileTime LastAccessTime;
+
+			public FileTime LastWriteTime;
+
+			public uint VolumeSerialNumber;
+
+			public uint FileSizeHigh;
+
+			public uint FileSizeLow;
+
+			public uint NumberOfLinks;
+
+			public uint FileIndexHigh;
+
+			public uint FileIndexLow;
+		}
+	}
+}
