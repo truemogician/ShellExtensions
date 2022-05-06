f82e8df6dbf044a2cf8f0f8e9f87a1ff9e9a4f18
true_mogician <truemogician@gmail.com>
2022-05-06 18:15:58 +0800

    feat(FolderWrapper): add project FolderWrapper

---
+++ Miscellany/FolderWrapper/FolderWrapperException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FolderWrapper;
+
+public class FolderWrapperException : Exception {
+	public FolderWrapperException() : base() { }
+
+	public FolderWrapperException(string message = null) : base(message) { }
+}
