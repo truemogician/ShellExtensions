c62f4545e9732ba45d958d58e7976e5c15622c9a
true_mogician <925269713@qq.com>
2022-05-27 00:56:31 +0800

    feat: better menu hierachy

--- Miscellany/SymbolLinkTool/Properties/AssemblyInfo.cs
+++ Miscellany/SymbolLinkTool/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SymbolLinkTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows COM server providing context menus to create and manage file hard links")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("true_mogician")]
 [assembly: AssemblyProduct("SymbolLinkTool")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 [assembly: NeutralResourcesLanguage("")]
