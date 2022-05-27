cf092dcdfde58efd83281507b148d933853e6f46
true_mogician <truemogician@gmail.com>
2022-05-27 18:27:19 +0800

    feat(DllRegistrator): better menu hierachy

--- Registration/DllRegistrator/ContextMenu.cs
+++ Registration/DllRegistrator/ContextMenu.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -23,21 +26,19 @@ internal enum RegistrationStatus {
 
 	UnregisteredOnOS32Bit = 2,
 
-	PartialOnOS32Bit = 3,
-
 	RegisteredOnOS64Bit = 4,
 
-	UnregisteredOnOS64Bit = 8,
-
-	PartialOnOS64Bit = 12
+	UnregisteredOnOS64Bit = 8
 }
 
 [ComVisible(true)]
 [COMServerAssociation(AssociationType.ClassOfExtension, ".dll")]
 public class ContextMenu : SharpContextMenu {
+	private static Image Icon { get; } = new Bitmap(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Icon.png"));
+
 	protected override bool CanShowMenu() => SelectedItemPaths.Count() == 1;
 
-	protected override ContextMenuStrip CreateMenu() {
+	protected override ContextMenuStrip? CreateMenu() {
 		string path = SelectedItemPaths.First();
 		List<ServerEntry> entries;
 		try {
@@ -47,44 +48,68 @@ public class ContextMenu : SharpContextMenu {
 			return null;
 		}
 		var menu = new ContextMenuStrip();
-		var item = new ToolStripMenuItem("注册类库");
-		var status = RegistrationStatus.Unknown;
-		if (entries.Count != 0)
-			status = entries.Select(entry => ServerRegistrationManager.GetServerRegistrationInfo(entry.Server.ServerClsid, RegistrationType.OS32Bit)).Aggregate(status, (current, info) => current | (info == null ? RegistrationStatus.UnregisteredOnOS32Bit : RegistrationStatus.RegisteredOnOS32Bit));
-		else
-			status |= RegistrationStatus.UnregisteredOnOS32Bit;
-		if (Environment.Is64BitOperatingSystem) {
-			if (entries.Count != 0)
-				status = entries.Select(entry => ServerRegistrationManager.GetServerRegistrationInfo(entry.Server.ServerClsid, RegistrationType.OS64Bit)).Aggregate(status, (current, info) => current | (info == null ? RegistrationStatus.UnregisteredOnOS64Bit : RegistrationStatus.RegisteredOnOS64Bit));
+		var status = entries.Count == 0
+			? RegistrationStatus.UnregisteredOnOS32Bit
+			: entries.Select(
+					entry => ServerRegistrationManager.GetServerRegistrationInfo(
+						entry.Server.ServerClsid,
+						RegistrationType.OS32Bit
+					)
+				)
+				.Aggregate(RegistrationStatus.Unknown, (current, info) => current | (info is null ? RegistrationStatus.UnregisteredOnOS32Bit : RegistrationStatus.RegisteredOnOS32Bit));
+		if (Environment.Is64BitOperatingSystem)
+			status |= entries.Count == 0
+				? RegistrationStatus.UnregisteredOnOS64Bit
+				: entries.Select(
+						entry => ServerRegistrationManager.GetServerRegistrationInfo(
+							entry.Server.ServerClsid,
+							RegistrationType.OS64Bit
+						)
+					)
+					.Aggregate(status, (current, info) => current | (info == null ? RegistrationStatus.UnregisteredOnOS64Bit : RegistrationStatus.RegisteredOnOS64Bit));
+		bool status32 = status.HasFlag(RegistrationStatus.UnregisteredOnOS32Bit);
+		bool status64 = status.HasFlag(RegistrationStatus.UnregisteredOnOS64Bit);
+		if (status32 || status64) {
+			if (status32 && status64)
+				menu.Items.Add(
+					new ToolStripMenuItem("注册类库", Icon) {
+						DropDownItems = {
+							new ToolStripMenuItem("到当前系统", null, (_, _) => Register("x64")),
+							new ToolStripMenuItem("到32位子系统", null, (_, _) => Register("x86"))
+						}
+					}
+				);
+			else if (status64 || !Environment.Is64BitOperatingSystem)
+				menu.Items.Add(new ToolStripMenuItem("注册类库", Icon, (_, _) => Register()));
 			else
-				status |= RegistrationStatus.UnregisteredOnOS64Bit;
-			if (!status.HasFlag(RegistrationStatus.UnregisteredOnOS32Bit) && !status.HasFlag(RegistrationStatus.UnregisteredOnOS64Bit))
-				item.Text = "注销类库";
-			if (status.HasFlag(RegistrationStatus.UnregisteredOnOS64Bit))
-				item.DropDownItems.Add(new ToolStripMenuItem("注册", null, (_, _) => Register("r", "x64")));
-			if (status.HasFlag(RegistrationStatus.RegisteredOnOS64Bit))
-				item.DropDownItems.Add(new ToolStripMenuItem("注销", null, (_, _) => Register("u", "x64")));
-			if (status.HasFlag(RegistrationStatus.UnregisteredOnOS32Bit))
-				item.DropDownItems.Add(new ToolStripMenuItem("注册到32位子系统", null, (_, _) => Register("r", "x86")));
-			if (status.HasFlag(RegistrationStatus.RegisteredOnOS32Bit))
-				item.DropDownItems.Add(new ToolStripMenuItem("从32位子系统注销", null, (_, _) => Register("u", "x86")));
+				menu.Items.Add(new ToolStripMenuItem("注册类库（到32位子系统）", Icon, (_, _) => Register("x86")));
 		}
-		else {
-			if (status == RegistrationStatus.PartialOnOS32Bit) {
-				item.DropDownItems.Add(new ToolStripMenuItem("注册", null, (_, _) => Register("r", "x86")));
-				item.DropDownItems.Add(new ToolStripMenuItem("注销", null, (_, _) => Register("u", "x86")));
-			}
-			else {
-				item.Click += (_, _) => Register(status == RegistrationStatus.UnregisteredOnOS32Bit ? "r" : "u", "x86");
-				if (status == RegistrationStatus.RegisteredOnOS32Bit)
-					item.Text = "注销类库";
-			}
+		status32 = status.HasFlag(RegistrationStatus.RegisteredOnOS32Bit);
+		status64 = status.HasFlag(RegistrationStatus.RegisteredOnOS64Bit);
+		if (status32 || status64) {
+			if (status32 && status64)
+				menu.Items.Add(
+					new ToolStripMenuItem("注销类库", Icon) {
+						DropDownItems = {
+							new ToolStripMenuItem("从当前系统", null, (_, _) => Unregister("x64")),
+							new ToolStripMenuItem("从32位子系统", null, (_, _) => Unregister("x86"))
+						}
+					}
+				);
+			else if (status64 || !Environment.Is64BitOperatingSystem)
+				menu.Items.Add(new ToolStripMenuItem("注销类库", Icon, (_, _) => Unregister()));
+			else
+				menu.Items.Add(new ToolStripMenuItem("注销类库（从32位子系统）", Icon, (_, _) => Unregister("x86")));
 		}
-		menu.Items.Add(item);
 		return menu;
 	}
 
-	protected void Register(string action, string targetArch) {
+	private void Register(string? targetArch = null) => Operate("r", targetArch);
+
+	private void Unregister(string? targetArch = null) => Operate("u", targetArch);
+
+	private void Operate(string action, string? targetArch = null) {
+		targetArch ??= Environment.Is64BitOperatingSystem ? "x64" : "x86";
 		string registrationHandler = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "RegistrationHandler.exe");
 		Process.Start(
 			new ProcessStartInfo {
