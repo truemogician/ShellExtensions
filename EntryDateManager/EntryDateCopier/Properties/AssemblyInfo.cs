6dedb55afe71408a0d81a0cfa31d5ef5cd93d217
true_mogician <truemogician@gmail.com>
2022-07-31 13:33:59 +0800

    fix: config update

--- EntryDateManager/EntryDateCopier/Properties/AssemblyInfo.cs
+++ EntryDateManager/EntryDateCopier/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EntryDateCopier")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Entry Date Copier")]
+[assembly: AssemblyDescription("A COM server for entry date management.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("true_mogician")]
 [assembly: AssemblyProduct("EntryDateCopier")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
 [assembly: NeutralResourcesLanguage("zh-Hans")]
