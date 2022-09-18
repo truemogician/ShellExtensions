d48c5fc70e22f65d1b72ed7908fed425424593a0
true_mogician <truemogician@gmail.com>
2022-09-18 21:14:10 +0800

    fix(RestartExplorer): explorer windows restore behavior
    refactor: clean up
    refactor: extract ExplorerInfo to separate file

---
+++ Utility/RestartExplorer/ExplorerInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using PInvoke;
+using SHDocVw;
+
+namespace RestartExplorer {
+	static partial class Program {
+		private class ExplorerInfo {
+			public string Location { get; }
+
+			public User32.WINDOWPLACEMENT Placement { get; set; }
+
+			public ExplorerInfo(InternetExplorer ie) {
+				var hWnd = new IntPtr(ie.HWND);
+				Location = ie.LocationURL;
+				Placement = User32.GetWindowPlacement(hWnd);
+				if (Placement.showCmd == User32.WindowShowStyle.SW_SHOWNORMAL) {
+					User32.GetWindowRect(hWnd, out var actual);
+					Placement = new User32.WINDOWPLACEMENT {
+						length = Placement.length,
+						flags = Placement.flags,
+						showCmd = Placement.showCmd,
+						ptMaxPosition = Placement.ptMaxPosition,
+						ptMinPosition = Placement.ptMinPosition,
+						rcNormalPosition = actual
+					};
+				}
+			}
+		}
+	}
+}
