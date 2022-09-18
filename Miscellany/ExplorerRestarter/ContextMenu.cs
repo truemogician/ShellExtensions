4676e0d52d5ac00ce4c6acdb39a3da7efdd75e09
true_mogician <truemogician@gmail.com>
2022-09-18 22:11:56 +0800

    feat(ExplorerRestarter): add project ExplorerRestarter

---
+++ Miscellany/ExplorerRestarter/ContextMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+
+namespace ExplorerRestarter;
+
+[ComVisible(true)]
+[COMServerAssociation(AssociationType.DesktopBackground)]
+[COMServerAssociation(AssociationType.DirectoryBackground)]
+public class ContextMenu : SharpContextMenu {
+	protected override bool CanShowMenu() => true;
+
+	protected override ContextMenuStrip CreateMenu() => new() {
+		Items = {
+			new ToolStripMenuItem(
+				"重启文件资源管理器",
+				Resource.Restart,
+				(_, _) => {
+					try {
+						Explorer.Restart();
+					}
+					catch (Exception ex) {
+						MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			)
+		}
+	};
+}
