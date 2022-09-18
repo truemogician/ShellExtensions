b4c1aa9074a32551c987c940c6b8f4f125bb0b03
true_mogician <truemogician@gmail.com>
2022-09-18 21:14:10 +0800

    fix: explorer windows restore behavior
    refactor: clean up
    refactor: extract ExplorerInfo to separate file

--- Utility/RestartExplorer/Program.cs
+++ Utility/RestartExplorer/Program.cs
@@ -7,89 +7,70 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace RestartExplorer {
-	class Program {
+	static partial class Program {
 		[DllImport("user32.dll", SetLastError = true)]
-		static extern bool PostMessage(IntPtr hWnd, [MarshalAs(UnmanagedType.U4)] uint Msg, IntPtr wParam, IntPtr lParam);
+		static extern bool PostMessage(IntPtr hWnd, [MarshalAs(UnmanagedType.U4)] uint msg, IntPtr wParam, IntPtr lParam);
+
 		const int WM_USER = 0x0400;
-		class ExplorerInfo {
-			public string Location { get; }
-			public User32.WINDOWPLACEMENT Placement { get; set; }
-			public bool IsUsed { get; set; }
-			public ExplorerInfo(InternetExplorer ie) {
-				IntPtr hWnd = new IntPtr(ie.HWND);
-				Location = ie.LocationURL;
-				Placement = User32.GetWindowPlacement(hWnd);
-				if (Placement.showCmd == User32.WindowShowStyle.SW_SHOWNORMAL) {
-					User32.GetWindowRect(hWnd, out RECT actual);
-					Placement = new User32.WINDOWPLACEMENT {
-						length = Placement.length,
-						flags = Placement.flags,
-						showCmd = Placement.showCmd,
-						ptMaxPosition = Placement.ptMaxPosition,
-						ptMinPosition = Placement.ptMinPosition,
-						rcNormalPosition = actual
-					};
-				}
-				IsUsed = false;
-			}
-		}
-		static void Main() {
-			List<ExplorerInfo> explorersInfo = new List<ExplorerInfo>();
-			var shells = new ShellWindows().Cast<object>().ToArray();
-			for (int i = 0; i < shells.Length; ++i) {
-				InternetExplorer ie = (InternetExplorer)shells[i];
-				if (Path.GetFileNameWithoutExtension(ie.FullName).ToLower() == "explorer") {
-					var hWnd = new IntPtr(ie.HWND);
-					explorersInfo.Add(new ExplorerInfo(ie));
-					User32.SendMessage(hWnd, User32.WindowMessage.WM_CLOSE, (IntPtr)0, (IntPtr)0);
-				}
+
+		static async Task Main() {
+			var explorerInfos = new List<ExplorerInfo>();
+			var shells = new ShellWindows().Cast<InternetExplorer>().ToArray();
+			foreach (var ie in shells) {
+				if (Path.GetFileNameWithoutExtension(ie.FullName).ToLower() != "explorer")
+					continue;
+				var hWnd = new IntPtr(ie.HWND);
+				explorerInfos.Add(new ExplorerInfo(ie));
+				User32.SendMessage(hWnd, User32.WindowMessage.WM_CLOSE, (IntPtr)0, (IntPtr)0);
 			}
 			try {
 				var ptr = User32.FindWindow("Shell_TrayWnd", null);
 				PostMessage(ptr, WM_USER + 436, (IntPtr)0, (IntPtr)0);
-				do {
+				while (true) {
 					ptr = User32.FindWindow("Shell_TrayWnd", null);
 					if (ptr.ToInt32() == 0)
 						break;
-					Thread.Sleep(1000);
-				} while (true);
+					await Task.Delay(1000);
+				}
 			}
 			catch (Exception ex) {
-				Console.WriteLine($"{ex.Message} {ex.StackTrace}");
+				await Console.Error.WriteLineAsync($"{ex.Message} {ex.StackTrace}");
 			}
-			Process.Start(new ProcessStartInfo {
-				FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "explorer.exe"),
-				UseShellExecute = true
-			});
-			foreach (var info in explorersInfo) {
-				var cmd = Process.Start(new ProcessStartInfo {
-					FileName = "cmd",
-					UseShellExecute = false,
-					CreateNoWindow = true,
-					RedirectStandardInput = true
-				});
-
-				cmd.StandardInput.WriteLine(string.Format("start /min \"\" \"{0}\"", string.IsNullOrEmpty(info.Location) ? "userinit" : info.Location));
-				cmd.StandardInput.WriteLine("exit");
-				cmd.WaitForExit();
-			}
-			shells = new ShellWindows().Cast<object>().ToArray();
-			while (shells.Length < explorersInfo.Count) {
-				Thread.Sleep(1000);
-				shells = new ShellWindows().Cast<object>().ToArray();
-			}
-			for (int i = 0; i < shells.Length; ++i) {
-				InternetExplorer ie = (InternetExplorer)shells[i];
-				if (Path.GetFileNameWithoutExtension(ie.FullName).ToLower() == "explorer") {
-					try {
-						ExplorerInfo info = explorersInfo.First((ExplorerInfo expInfo) => !expInfo.IsUsed && ie.LocationURL == expInfo.Location);
-						info.IsUsed = true;
-						IntPtr hWnd = new IntPtr(ie.HWND);
-						User32.SetWindowPlacement(hWnd, info.Placement);
-					}
-					catch { }
+			var explorerPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "explorer.exe");
+			Process.Start(
+				new ProcessStartInfo {
+					FileName = explorerPath,
+					UseShellExecute = false
+				}
+			);
+			if (explorerInfos.Count > 0) {
+				foreach (var info in explorerInfos) {
+					Process.Start(
+						new ProcessStartInfo {
+							FileName = explorerPath,
+							Arguments = $"\"{info.Location}\"",
+							UseShellExecute = true,
+							WindowStyle = ProcessWindowStyle.Minimized
+						}
+					);
+				}
+				shells = new ShellWindows().Cast<InternetExplorer>().ToArray();
+				while (shells.Length < explorerInfos.Count) {
+					Thread.Sleep(1000);
+					shells = new ShellWindows().Cast<InternetExplorer>().ToArray();
+				}
+				foreach (var ie in shells) {
+					if (Path.GetFileNameWithoutExtension(ie.FullName).ToLower() != "explorer")
+						continue;
+					var current = explorerInfos.FirstOrDefault(info => ie.LocationURL == info.Location);
+					if (current is null)
+						continue;
+					explorerInfos.Remove(current);
+					var hWnd = new IntPtr(ie.HWND);
+					User32.SetWindowPlacement(hWnd, current.Placement);
 				}
 			}
 		}
